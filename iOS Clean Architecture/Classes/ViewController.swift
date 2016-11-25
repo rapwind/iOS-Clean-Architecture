ae7d8fa458ebf00648921a22ffdb3ce1b16b73db
はやみん <dev@hayam.in>
2016-11-25 19:10:41 +0900

    Initial Commit

---
+++ iOS Clean Architecture/Classes/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  iOS Clean Architecture
+//
+//  Created by hayamin on 11/25/16.
+//  Copyright © 2016 in.hayam. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
